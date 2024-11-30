7293219a7dae21732d3290bbba4fea9e27f99158
Poomyyyy <8pitipoomsaripan@gmail.com>
2024-11-30 16:56:36 +0700

    update

--- api/connect.php
+++ api/connect.php
@@ -4,10 +4,10 @@
         $db_user = "root";
         $db_password = "";
 
-
 try {
     $db = new PDO("mysql:host={$db_host}; dbname={$db_name}", $db_user, $db_password);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //echo "pass";
 } catch (PDOException $e) {
     $e->getMessage();
 }
